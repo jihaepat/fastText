f5197efb456c6d6b41608046d2a0ab0461b9fbb1
leehyunsoo <lhs950204@naver.com>
2018-04-13 17:37:57 +0900

    full_build 수정 (기존 make & build 에서 pip install . 으로 변경)

--- full_build_install.sh
+++ full_build_install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-make clean
-make
-python setup.py clean --all
-python setup.py build
-python setup.py install
+#make clean
+#make
+#python setup.py clean --all
+#python setup.py build
+#python setup.py install
+
+#기존에 fasttext를 지우고 fasttext에서 제설치
+pip uninstall fasttext -y
+pip install .
